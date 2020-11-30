563b57f22649d16f994428c346caa80e43e2c56d
Francisco Tavares De Lima Neto <francisco.tln@outlook.com>
2020-11-30 12:30:59 +0100

    hydrate lower elements when at the bottom of the page and clicking back/forward browsers button

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ export default class LazyHydrate extends Component {
 
   componentDidMount() {
     const { lazy = true } = this.props;
-    if (lazy) {
-      this.observer = new IntersectionObserver((entries, observerRef) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting || entry.intersectionRatio > 0) {
-            this.setState({ hydrated: true });
-            observerRef.disconnect();
-          }
-        });
-      }, { rootMargin: this.props.rootMargin || '200px' });
+    const hydrate = () => this.setState({ hydrated: true });
+    if (this.ref.current.childElementCount) {
+      if (lazy) {
+        this.observer = new IntersectionObserver((entries, observerRef) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting || entry.intersectionRatio > 0) {
+              hydrate();
+              observerRef.disconnect();
+            }
+          });
+        }, { rootMargin: this.props.rootMargin || '200px' });
 
-      this.observer.observe(this.ref.current);
+        this.observer.observe(this.ref.current);
+      }
+    } else {
+      hydrate();
     }
   }
 
